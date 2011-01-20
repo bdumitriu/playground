deef3c7176a2985a7e804739fb95fab2d0e99738
Bogdan Dumitriu <bogdan.dumitriu@qtronic-software.ro>
2011-01-20 10:31:40 +0200

    ICPC 2011 Challenge: more tests.

--- haskell/icypc/haskell/GameInfoTests.hs
+++ haskell/icypc/haskell/GameInfoTests.hs
@@ -5,13 +5,6 @@ import Test.HUnit
 import GameStateParser
 import GameInfo
 
-{-
-, emptyGameInfo
-, registerNewState
-, currentState
-, currentBareState
--}
-
 stateAtTurn0 = parseGameState . unlines $ [
     "0",
     "0 0",
@@ -55,11 +48,54 @@ stateAtTurn0 = parseGameState . unlines $ [
     "*",
     "*"]
 
+stateAtTurn1 = parseGameState . unlines $ [
+    "179",
+    "0 690",
+    "** ** 3a 3a 3i 3a 3a 3a 3a 3a 2a 3a ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "** 3a 3a 3a 0a 3a 3a 3a 3a 0a 0a 0a ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "3a 3a 3a 0f 3a 3a 3a 3a 3a 0a 3a 0a ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "3a 3a 3a 2g 3a 3a 3a 3a 3a 0a 0a 2a ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "3a 3a 3a 3h 3a 3a 3a 3a 3a 3a 3a ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "5c 1d 6e 3a 3a 3a 3a 3a 3a 3a 3a ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "3a 3a 3a 3a 2a 3a 3a 3a 3a 3a ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "3a 3a 3a 3a 3a 0a 0a 0a 3a 3a ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "3a 3a 3a 3a 3a 0a 2a 0j 3a ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "3a 3a 3a 0a 0a 0a 0a 0a ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "3a 3a 3a 0a 3a 0a ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "3a 3a 3a 0a ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **",
+    "0 4 C i 0",
+    "*",
+    "3 2 S g 0",
+    "4 0 C h 2",
+    "10 4 C b 0",
+    "7 5 C e 0",
+    "*",
+    "2 10 S c 0"]
+
 testRegisterFirstState = TestCase (do let gameInfo  = registerNewState emptyGameInfo stateAtTurn0
+                                          bareState = currentBareState gameInfo
+                                          state     = currentState gameInfo
                                           f         = field state
                                           reds      = redChildren state
-                                          state     = currentState gameInfo
-                                          bareState = currentBareState gameInfo
                                       assertEqual "bare state is registered state" stateAtTurn0 bareState
                                       assertEqual "augmented state in first turn has info age 0 for all known field spaces" True (checkAllInfoAgesAreZero f)
                                       assertEqual "red child 1 in field @ 0 2" (getChild1 reds) (childIn (f!(0, 2)))
@@ -72,8 +108,27 @@ testRegisterFirstState = TestCase (do let gameInfo  = registerNewState emptyGame
 checkAllInfoAgesAreZero :: Field -> Bool
 checkAllInfoAgesAreZero = null . filter (\fieldSpace -> infoAge fieldSpace /= 0) . elems
 
+testRegisterSecondState = TestCase (do let gameInfo  = registerNewState (registerNewState emptyGameInfo stateAtTurn0) stateAtTurn1
+                                           bareState = currentBareState gameInfo
+                                           state     = currentState gameInfo
+                                           f         = field state
+                                           reds      = redChildren state
+                                           blues     = blueChildren state
+                                       assertEqual "bare state is registered state" stateAtTurn1 bareState
+                                       assertEqual "augmented state in first turn has info age 1 for space (0, 1)" 1 (infoAge (f!(0, 1)))
+                                       assertEqual "augmented state in first turn has info age 0 for space (2, 3)" 0 (infoAge (f!(2, 3)))
+                                       assertEqual "augmented state in first turn has correct info for space (0, 1)" (Empty 3) (contents (f!(0, 1)))
+                                       assertEqual "red child 1 in field @ 0 4" (getChild1 reds) (childIn (f!(0, 4)))
+                                       assertEqual "red child 3 in field @ 3 2" (getChild3 reds) (childIn (f!(3, 2)))
+                                       assertEqual "red child 4 in field @ 4 0" (getChild4 reds) (childIn (f!(4, 0)))
+                                       assertEqual "blue child 1 in field @ 10 4" (getChild1 blues) (childIn (f!(10, 4)))
+                                       assertEqual "blue child 2 in field @ 7 5" (getChild2 blues) (childIn (f!(7, 5)))
+                                       assertEqual "blue child 4 in field @ 2 10" (getChild4 blues) (childIn (f!(2, 10)))
+                                   )
+
 tests = TestList [
            TestLabel "testRegisterFirstState" testRegisterFirstState
+         , TestLabel "testRegisterSecondState" testRegisterSecondState
         ]
 
 main = runTestTT tests
--- haskell/icypc/haskell/GameStateParserTests.hs
+++ haskell/icypc/haskell/GameStateParserTests.hs
@@ -104,12 +104,63 @@ testInitialState = TestCase (do let gameState = parseGameState sampleInitialStat
 testSampleState1 = TestCase (do let gameState = parseGameState sampleState1
                                     f         = field gameState
                                     reds      = redChildren gameState
+                                    Just r1   = getChild1 reds
+                                    Just r2   = getChild2 reds
+                                    Just r3   = getChild3 reds
+                                    Just r4   = getChild4 reds
                                     blues     = blueChildren gameState
+                                    Just b1   = getChild1 blues
+                                    Just b2   = getChild2 blues
+                                    Just b4   = getChild4 blues
+
                                 assertEqual "state turn is 179" 179 (turnNumber gameState)
                                 assertEqual "scores is 690 to 0" (0, 690) (scoreRed gameState, scoreBlue gameState)
+
                                 assertEqual "field with 'c'" (S_SnowBall 5) (contents ((field gameState)!(5, 0)))
+                                assertEqual "field with 'd'" (M_SnowBall 1) (contents ((field gameState)!(5, 1)))
+                                assertEqual "field with 'e'" (MS_SnowBall 6) (contents ((field gameState)!(5, 2)))
+                                assertEqual "field with 'f'" (L_SnowBall 0) (contents ((field gameState)!(2, 3)))
+                                assertEqual "field with 'g'" (LM_SnowBall 2) (contents ((field gameState)!(3, 3)))
+                                assertEqual "field with 'h'" (LS_SnowBall 3) (contents ((field gameState)!(4, 3)))
+                                assertEqual "field with 'i'" (RedSnowman 3) (contents ((field gameState)!(0, 4)))
+                                assertEqual "field with 'j'" (BlueSnowman 0) (contents ((field gameState)!(8, 7)))
+
+                                assertEqual "1st red child is in pos (0, 4)" (0, 4) (position r1)
+                                assertEqual "2nd red child is in pos (2, 3)" (2, 3) (position r2)
+                                assertEqual "3rd red child is in pos (3, 2)" (3, 2) (position r3)
+                                assertEqual "4th red child is in pos (4, 0)" (4, 0) (position r4)
+
+                                assertEqual "1st blue child is in pos (10, 4)" (10, 4) (position b1)
+                                assertEqual "2nd blue child is in pos (7, 5)" (7, 5) (position b2)
+                                assertEqual "3rd blue child is invisible" Nothing (getChild3 blues)
+                                assertEqual "4th blue child is in pos (2, 10)" (2, 10) (position b4)
+
+                                assertEqual "1st red child is crouching" False (isStanding r1)
+                                assertEqual "2nd red child is standing" True (isStanding r2)
+                                assertEqual "3rd red child is standing" True (isStanding r3)
+                                assertEqual "4th red child is crouching" False (isStanding r4)
+
+                                assertEqual "1st blue child is crouching" False (isStanding b1)
+                                assertEqual "2nd blue child is crouching" False (isStanding b2)
+                                assertEqual "4th blue child is standing" True (isStanding b4)
+
+                                assertEqual "child with 'b'" (Snow 1) (payload b1)
+                                assertEqual "child with 'c'" (Snow 2) (payload b4)
+                                assertEqual "child with 'e'" (SSnowBall 1) (payload b2)
+                                assertEqual "child with 'f'" (SSnowBall 2) (payload r2)
+                                assertEqual "child with 'g'" (SSnowBall 3) (payload r3)
+                                assertEqual "child with 'h'" (MSnowBall) (payload r4)
+                                assertEqual "child with 'i'" (LSnowBall) (payload r1)
+
+                                assertEqual "1st red child is not dazed" 0 (dazedAnother r1)
+                                assertEqual "2nd red child is dazed for 4" 4 (dazedAnother r2)
+                                assertEqual "3rd red child is not dazed" 0 (dazedAnother r3)
+                                assertEqual "4th red child is dazed for 2" 2 (dazedAnother r4)
                             )
 
-tests = TestList [TestLabel "testInitialState" testInitialState]
+tests = TestList [
+           TestLabel "testInitialState" testInitialState
+         , TestLabel "testSampleState1" testSampleState1
+        ]
 
 main = runTestTT tests

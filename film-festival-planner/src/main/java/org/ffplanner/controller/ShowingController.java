417971c37513c377ede8c04059ea17b1c5efee66
Bogdan Dumitriu <bogdan.dumitriu@qtronic-software.ro>
2011-12-11 12:01:25 +0100

    Non working code for navigation to next/previous day.

--- film-festival-planner/src/main/java/org/ffplanner/controller/ShowingController.java
+++ film-festival-planner/src/main/java/org/ffplanner/controller/ShowingController.java
@@ -1,18 +1,15 @@
 package org.ffplanner.controller;
 
-import java.util.Collection;
-import java.util.Date;
-import javax.inject.Named;
-import java.io.Serializable;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 import org.ffplanner.ShowingEJB;
 import org.ffplanner.entity.Showing;
 import org.ffplanner.entity.Venue;
@@ -25,6 +22,8 @@ import org.ffplanner.entity.Venue;
 @RequestScoped
 public class ShowingController implements Serializable {
 
+	private static final SimpleDateFormat DAY_FORMAT = new SimpleDateFormat("dd/MM/yyyy");
+
 	@EJB
 	private ShowingEJB showingEJB;
 
@@ -33,6 +32,8 @@ public class ShowingController implements Serializable {
 	private Collection<Venue> venues;
 
 	private Collection<Hour> hours;
+
+	private Date day;
 
 	public ShowingController() {
 		hours = new ArrayList<>();
@@ -62,9 +63,39 @@ public class ShowingController implements Serializable {
 		return hourSlots;
 	}
 
+	public String getPreviousDay() {
+		final GregorianCalendar calendar = new GregorianCalendar();
+		calendar.setTime(day);
+		calendar.roll(Calendar.DAY_OF_MONTH, false);
+		return DAY_FORMAT.format(calendar.getTime());
+	}
+
+	public String getNextDay() {
+		final GregorianCalendar calendar = new GregorianCalendar();
+		calendar.setTime(day);
+		calendar.roll(Calendar.DAY_OF_MONTH, true);
+		return DAY_FORMAT.format(calendar.getTime());
+	}
+
+	public void setDay(Date day) {
+		this.day = day;
+	}
+
+	public String x() {
+		return "index";
+	}
+
 	public String prepareShowings() {
-		final GregorianCalendar calendar = new GregorianCalendar(2011, Calendar.JUNE, 3);
-		final Collection<Showing> showings = showingEJB.getShowingsFor(calendar.getTime());
+		final ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+		final String day = externalContext.getRequestParameterMap().get("day");
+		try {
+			final Date date = DAY_FORMAT.parse(day);
+			setDay(date);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		final Collection<Showing> showings = showingEJB.getShowingsFor(this.day);
 		showingsByVenuesAndHours = new HashMap<>();
 		venues = new LinkedList<>();
 		Map<Integer, Showing> showingsByHour = new HashMap<>();
@@ -85,5 +116,10 @@ public class ShowingController implements Serializable {
 		}
 		showingsByVenuesAndHours.put(currentVenue, showingsByHour);
 		return "DaySchedule";
+	}
+
+	public static void main(String[] args) throws ParseException {
+		final Date day = DAY_FORMAT.parse("03/06/2011");
+		System.out.println(day);
 	}
 }
